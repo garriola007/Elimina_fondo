f54fbe3c953f5f10615e730eeac3764be19b6c8a
garriola007 <gustavo.arriola@gmail.com>
2024-09-12 18:38:11 -0300

    librerias

--- app.py
+++ app.py
@@ -7,17 +7,38 @@ def save_upload_file(upload_file):
   upload_dir = "uploads"
   if not os.path.exists(upload_dir):
     os.makedirs(upload_dir)
-  file_path = os.path.join(upload_dir, uploaded_file.name)
+  file_path = os.path.join(upload_dir, upload_file.name)
   with open(file_path, "wb") as f:
-    f.write(uploaded_file.getbuffer())
+    f.write(upload_file.getbuffer())
   return file_path
 
 def run_background_remover(input_img_file):
-  input_img_path = save_uploaded_file(input_img_file)
-  output_img_path = input_img_path.replace(_old:'.', __new:'__rmbg.').replace(__old:'jpeg', __new:'png')
+  input_img_path = save_upload_file(input_img_file)
+  output_img_path = input_img_path.replace('.', '__rmbg.').replace('jpg', 'png').replace('jpeg', 'png') 
   try:
     image = Image.open(input_img_path)
+    output = remove(image)
+    output.save(output_img_path,  format='PNG')
+    col1, col2 = st.columns(2)
+    with col1:
+      st.header('Antes')
+      st.image(input_img_file, caption='Original')
+      with open(input_img_path, 'rb') as image_file:
+        st.download_button(Label="Descargar imagen original", data=image_file, file_name=os.path.basename(input_img_path), mime='image/jpeg'
+                           )
+    with col2:
+      st.header('Despues')
+      st.image(input_img_file, caption='Imagen con fondo removido')
+      with open(input_img_path, 'rb') as image_file:
+        st.download_button(Label="Descargar imagen procesada", data=image_file, file_name=os.path.basename(output_img_path), mime='image/jpeg'  
+                           )
+    st.success('Fondo removido exitosamente')
   
+  except Exception as e:
+    st.error('Error al remover fondo, intente nuevamente')
+    st.write(e)
+
+def main():
+  st.title('Removedor de fondos de imagenes')
   
-    
   
